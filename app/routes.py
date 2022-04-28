91aae98bbcf77092c7234d4b8b86877939fe58aa
gleb <gleb.omarov@gmail.com>
2022-04-28 20:17:21 +0300

    first commit

--- app/routes.py
+++ app/routes.py
@@ -34,16 +34,29 @@ def login_post():
     return redirect(url_for('test'))
 
 
-@process_app.route('/signup', methods=['GET', 'POST'])
+@process_app.route('/signup')
 def signup():
+    form = AddEmployee()
+    return render_template('signup.html', form=form)
+
+
+@process_app.route('/signup', methods=['POST'])
+def signup_post():
     form = AddEmployee()
     if form.validate_on_submit():
         new_user = models.Employee(first_name=form.first_name.data,
                                    last_name=form.last_name.data,
                                    user_name=form.user_name.data,
-                                   password=form.password.data,
+                                   password=generate_password_hash(form.password.data, method='sha256'),
                                    admin=form.admin.data)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for("test"))
     return render_template('signup.html', form=form)
+
+
+@process_app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('test'))
